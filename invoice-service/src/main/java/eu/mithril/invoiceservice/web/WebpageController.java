530ad84c1629d3167aa85c8d8c268b94a0369209
Marko Šutić <marko.sutic@mithril.eu>
2025-02-12 15:00:13 +0100

    invoice-service spring thymeleaf

--- invoice-service/src/main/java/eu/mithril/invoiceservice/web/WebpageController.java
+++ invoice-service/src/main/java/eu/mithril/invoiceservice/web/WebpageController.java
@@ -1,13 +1,22 @@
 package eu.mithril.invoiceservice.web;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebpageController {
 
     @GetMapping("/")
-    public String homepage() {
+    public String homepage(
+            Model model,
+            @RequestParam(required = false, defaultValue = "stranger") String username
+    ) {
+        model.addAttribute("username", username);
+        model.addAttribute("currentDate", LocalDateTime.now());
         return "index.html";
     }
 }
